43a9e8e36392617777bc5cb3db098cd0817ecd8b
fajaryumna <fajaryumna2772@gmail.com>
2025-01-19 17:30:01 +0700

    initial commit

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class BaseController extends Controller
+{   
+    public function __construct()
+    {
+        Carbon::setLocale('id');
+    }
+
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($message, $result = null)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+        ];
+
+        if (!is_null($result)) {
+            $response['data'] = $result;
+        }
+
+        return response()->json($response, 200);
+    }
+  
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+  
+        if(!empty($errorMessages)){
+            $response['errors'] = $errorMessages;
+        }
+  
+        return response()->json($response, $code);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {   
+        // Membuat role untuk pengguna
+        $adminRole = Role::create(['name' => 'admin']);
+        $customerRole = Role::create(['name' => 'customer']);
+
+        // Membuat user admin
+        $userAdmin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin12345'),
+        ]);
+
+        // Membuat user customer
+        $userCostumer = User::create([
+            'name' => 'Fajar',
+            'email' => 'fajaryumna@gmail.com',
+            'password' => Hash::make('fajaryumna'),
+        ]);
+        
+        // Assign role ke masing-masing user
+        $userAdmin->assignRole($adminRole);
+        $userCostumer->assignRole($customerRole);
+
+    }
+}
